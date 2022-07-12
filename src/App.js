692709ab994733cbc611178a7520b8169e33f47a
alexhkz <alexwebcode@gmail.com>
2022-07-12 19:50:31 +0300

    add props to cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const arr = [
+	{title: 'Мужские кроссовки Nike Blazer Mid Suede', price: '12 999', imageUrl: "/img/sneakers/1.jpg", id: "1"},
+	{title: 'Мужские кроссовки Nike Air Max 270', price: '15 600', imageUrl: "/img/sneakers/2.jpg", id: "2"},
+	{title: 'Мужские кроссовки Nike Blazer Mid Suede', price: '8499', imageUrl: "/img/sneakers/3.jpg", id: "3"},
+	{title: 'Мужские кроссовки Puma X Aka Boku Future Rider', price: '8999', imageUrl: "/img/sneakers/4.jpg", id: "4"},
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -21,9 +28,13 @@ function App() {
 			</div>
 			
 			<div className="d-flex">
-
-				<Card />
-				
+				{arr.map((obj) => (
+					<Card 
+						title={obj.title} 
+						price={obj.price} 
+						imageUrl={obj.imageUrl}
+						key={obj.id} />
+					))}
 			</div>
 
 		</div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({title, price, imageUrl}) => {
 	return (
 		<div>
 			<div className="card">
 				<div className="favorite">
 					<img src="/img/unliked.svg" alt="unliked" />
 				</div>
-				<img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers1" />
-				<h5>Мужские кроссовки Nike Blazer Mid Suede</h5>
+				<img width={133} height={112} src={imageUrl} alt="sneakers1" />
+				<h5>{title}</h5>
 				<div className="d-flex justify-between align-center">
 					<div className="d-flex flex-column">
 						<span>Цена:</span>
-						<b>12 999 руб.</b>
+						<b>{price}</b>
 					</div>
 					<button className="button">
 						<img width={11} height={11} src="/img/plus.svg" alt="plus" />
